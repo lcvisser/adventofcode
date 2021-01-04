09ebc1d877caa46725de204dc89988104b6a82d0
lcvisser <ludo.visser@gmail.com>
2021-01-04 16:04:10 +0100

    First attempt at day 13 part 2; funtionally correct but too slow

--- 2020/13/solution.py
+++ 2020/13/solution.py
@@ -5,11 +5,6 @@ input_file = sys.argv[1]
 with open(input_file) as f:
     data = f.read()
 
-# data = """
-# 939
-# 7,13,x,x,59,x,31,19
-# """
-
 # Part 1: find earliest bus
 notes = [x for x in data.split('\n') if len(x) > 0]
 ts = notes[0]
@@ -20,3 +15,40 @@ schedule = list(zip(bus_ids, time_to_wait))
 schedule.sort(key=lambda x: x[1])
 bus = schedule[0]
 print("First bus: {} (wait time: {}), number: {}".format(bus[0], bus[1], bus[0] * bus[1]))
+
+# Part 2: find timestamp t so that all busses listed depart at one minute intervals from t
+import time
+from math import prod
+
+
+# busses = "17,x,13,19"
+# busses = "67,7,59,61"
+# busses = "67,x,7,59,61"
+# busses = "67,7,x,59,61"
+# busses = "1789,37,47,1889"
+
+intervals = [int(v) if v != 'x' else -1 for v in busses.split(',')]
+offsets = list(range(len(intervals)))
+max_interval = max(intervals)
+max_offset = offsets[intervals.index(max_interval)]
+intervals, offsets = zip(*[(i, o) for i, o in zip(intervals, offsets) if i > 0])
+
+ts1 = max_interval
+ts2 = prod(intervals)
+match1 = False
+match2 = False
+t0 = time.monotonic()
+n = 0
+while not (match1 or match2):
+    ts1 += max_interval
+    ts2 -= max_interval
+    match1 = all(x == 0 for x in [-(ts1 + o - max_offset) % v for v, o in zip(intervals, offsets)])
+    match2 = all(x == 0 for x in [-(ts2 + o - max_offset) % v for v, o in zip(intervals, offsets)])
+    n += 1
+
+if match1:
+    ts = ts1 - max_offset
+elif match2:
+    ts = ts2 - max_offset
+print("First time for all busses: {}".format(ts))
+print(time.monotonic() - t0, n, match1, match2)
