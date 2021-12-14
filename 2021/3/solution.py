c58e4f839035fdaa64967668974671f8ae0821e0
lcvisser <ludo.visser@gmail.com>
2021-12-14 21:58:07 +0100

    Improved implementation

--- 2021/3/solution.py
+++ 2021/3/solution.py
@@ -5,38 +5,23 @@ input_file = sys.argv[1]
 with open(input_file) as f:
     data = f.read()
 
-# Part 1: power consumption
-length = None
-zeros_counts = None
-ones_counts = None
+numbers = []
 for number in data.split('\n'):
     number = number.strip()
+    if number:
+        numbers.append(number)
 
-    # Skip possible empty lines
-    if not number:
-        continue
-
-    # Initialize counters
-    if length is None:
-        length = len(number)
-        zeros_counts = [0] * length
-        ones_counts = [0] * length
-
-    # Parse number
-    for i, x in enumerate(number):
-        if x == '0':
-            zeros_counts[i] += 1
-        elif x == '1':
-            ones_counts[i] += 1
-        else:
-            print(f"Unknown value: `{x}`")
+# Part 1: power consumption
+bits_per_position = zip(*numbers)
+value_counts_per_position = [(n.count('0'), n.count('1')) for n in bits_per_position]
+zeros_counts, ones_counts = zip(*value_counts_per_position)
 
 # Determine gamma rate
 gamma_rate_binary = [1 if ones > zeros else 0 for zeros, ones in zip(zeros_counts, ones_counts)]
 gamma_rate = sum(n << i for i, n in enumerate(reversed(gamma_rate_binary)))
 
 # Epsilon rate is bitwise inverse
-epsilon_rate = (2 ** length - 1) ^ gamma_rate
+epsilon_rate = (2 ** len(numbers[0]) - 1) ^ gamma_rate
 
 # Answer
 print("gamma_rate={}, epsilon_rate={}, answer={}".format(gamma_rate, epsilon_rate, gamma_rate * epsilon_rate))
