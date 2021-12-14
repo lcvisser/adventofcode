58ca2270d5588df9a19fa5f60fd66c8e199ffcf0
lcvisser <ludo.visser@gmail.com>
2021-12-14 22:17:18 +0100

    Day 3, part 2

--- 2021/3/solution.py
+++ 2021/3/solution.py
@@ -25,3 +25,40 @@ epsilon_rate = (2 ** len(numbers[0]) - 1) ^ gamma_rate
 
 # Answer
 print("gamma_rate={}, epsilon_rate={}, answer={}".format(gamma_rate, epsilon_rate, gamma_rate * epsilon_rate))
+
+# Part 2: life support
+def reduce(remaining_numbers, keep, tie_condition, iteration=0):
+    # Check for final number reached
+    if len(remaining_numbers) == 1:
+        return remaining_numbers[0]
+
+    # Count bits values per position
+    bits_per_position = list(zip(*remaining_numbers))
+    zeros_counts = bits_per_position[iteration].count('0')
+    ones_counts = bits_per_position[iteration].count('1')
+
+    # Determine which one to keep
+    if keep == "most":
+        value_keep_zeros_gt_ones = '0'
+        value_keep_zeros_lt_ones = '1'
+    elif keep == "fewest":
+        value_keep_zeros_gt_ones = '1'
+        value_keep_zeros_lt_ones = '0'
+
+    # Filter the remaining numbers
+    if zeros_counts > ones_counts:
+        filtered_numbers = filter(lambda n: n[iteration] == value_keep_zeros_gt_ones, remaining_numbers)
+    elif zeros_counts < ones_counts:
+        filtered_numbers = filter(lambda n: n[iteration] == value_keep_zeros_lt_ones, remaining_numbers)
+    else:
+        filtered_numbers = filter(lambda n: n[iteration] == tie_condition, remaining_numbers)
+
+    # Filter recursively
+    return reduce(list(filtered_numbers), keep, tie_condition, iteration + 1)
+
+# Convert binary to integer
+oxygen_rate = int(reduce(numbers, keep="most", tie_condition='1'), base=2)
+carbon_rate = int(reduce(numbers, keep="fewest", tie_condition='0'), base=2)
+
+# Answer
+print("oxygen_rate={}, carbon_rate={}, answer={}".format(oxygen_rate, carbon_rate, oxygen_rate * carbon_rate))
