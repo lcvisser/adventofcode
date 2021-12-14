5d04712fd66b78da11f60b2549091ddb9f3b135c
lcvisser <ludo.visser@gmail.com>
2021-12-14 21:11:48 +0100

    Day 2, part 2

--- 2021/2/solution.py
+++ 2021/2/solution.py
@@ -29,3 +29,30 @@ for instruction in data.split('\n'):
 
 # Answer
 print("position={}, depth={}, answer={}".format(position, depth, position * depth))
+
+# Part 2: aiming
+position = 0
+depth = 0
+aim = 0
+for instruction in data.split('\n'):
+    # Skip possible empty lines
+    if not instruction.strip():
+        continue
+
+    # Parse instructions
+    direction, distance = instruction.split(' ')
+    distance = int(distance)
+
+    # Increment position/depth
+    if direction == "forward":
+        position += distance
+        depth += distance * aim
+    elif direction == "down":
+        aim += distance
+    elif direction == "up":
+        aim -= distance
+    else:
+        print(f"Unknown instruction: `{instruction}`")
+
+# Answer
+print("position={}, depth={}, answer={}".format(position, depth, position * depth))
