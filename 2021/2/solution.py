fc00341be2dd4d695f2f6a036238499b02cb4289
lcvisser <ludo.visser@gmail.com>
2021-12-14 21:07:12 +0100

    Day 2, part 1

---
+++ 2021/2/solution.py
@@ -0,0 +1,31 @@
+import sys
+
+# Read data
+input_file = sys.argv[1]
+with open(input_file) as f:
+    data = f.read()
+
+# Part 1: simple instructions
+position = 0
+depth = 0
+for instruction in data.split('\n'):
+    # Skip possible empty lines
+    if not instruction.strip():
+        continue
+
+    # Parse instructions
+    direction, distance = instruction.split(' ')
+    distance = int(distance)
+
+    # Increment position/depth
+    if direction == "forward":
+        position += distance
+    elif direction == "down":
+        depth += distance
+    elif direction == "up":
+        depth -= distance
+    else:
+        print(f"Unknown instruction: `{instruction}`")
+
+# Answer
+print("position={}, depth={}, answer={}".format(position, depth, position * depth))
