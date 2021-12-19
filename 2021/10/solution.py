32816d2f0e6d0b9f86b709d23ddbfca93076c79e
lcvisser <ludo.visser@gmail.com>
2021-12-19 22:13:02 +0100

    Day 10, part 2

--- 2021/10/solution.py
+++ 2021/10/solution.py
@@ -12,13 +12,20 @@ pairs = {
     '{': '}',
     '<': '>'
 }
-scores = {
+invalid_scores = {
     ')': 3,
     ']': 57,
     '}': 1197,
     '>': 25137
 }
-score = 0
+completion_scores = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4
+}
+invalid_score = 0
+completion_score_per_line = []
 for line in data.strip().split('\n'):
     stack = list()
     for c in line:
@@ -31,8 +38,24 @@ for line in data.strip().split('\n'):
                 stack.pop(-1)
             else:
                 # Illegal closing
-                score += scores[c]
+                invalid_score += invalid_scores[c]
                 break
+    else:
+        if stack:
+            # Line incomplete
+            completion_score = 0
+            while stack:
+                o = stack.pop()
+                c = pairs[o]
+                completion_score *= 5
+                completion_score += completion_scores[c]
+            completion_score_per_line.append(completion_score)
 
 # Part 1: illegal syntax score
-print(f"Score: {score}")
+print(f"Invalid score: {invalid_score}")
+
+# Part 2: autocompletion score
+completion_score_per_line.sort()
+number_complete_lines = len(completion_score_per_line)
+middle = number_complete_lines // 2
+print(f"Autocompletion score: {completion_score_per_line[middle]}")
