bcff96b579418e4637d1eb9d421602896a7910e2
Visser, Ludo <ludo.visser@cgi.com>
2022-12-01 08:46:13 +0100

    Part 2 of day 1

--- 2022/1/solution.py
+++ 2022/1/solution.py
@@ -8,7 +8,6 @@ with open(input_file) as f:
 # Parse data
 lines = data.strip().split('\n')
 
-# Part 1: maximum number of calories carried
 cals_per_elf = [0]
 elf_number = 0
 for line in lines:
@@ -19,5 +18,10 @@ for line in lines:
         cals = int(line)
         cals_per_elf[elf_number] += cals
 
+# Part 1: maximum number of calories carried
 max_cals = sorted(cals_per_elf)[-1]
 print(f"Max. number of calories carried: {max_cals}")
+
+# Part 2: sum of maximum number of calories carried of top three
+max_cals3 = sorted(cals_per_elf)[-3:]
+print(f"Max. number of calories carried by top 3: {sum(max_cals3)}")
