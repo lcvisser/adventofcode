28ffe8539d95b47d79b53bf0ccb215daea114147
Visser, Ludo <ludo.visser@cgi.com>
2022-12-01 08:37:14 +0100

    Part 1 of day 1

--- 2022/1/solution.py
+++ 2022/1/solution.py
@@ -8,3 +8,16 @@ with open(input_file) as f:
 # Parse data
 lines = data.strip().split('\n')
 
+# Part 1: maximum number of calories carried
+cals_per_elf = [0]
+elf_number = 0
+for line in lines:
+    if line.strip() == "":
+        cals_per_elf.append(0)
+        elf_number += 1
+    else:
+        cals = int(line)
+        cals_per_elf[elf_number] += cals
+
+max_cals = sorted(cals_per_elf)[-1]
+print(f"Max. number of calories carried: {max_cals}")
