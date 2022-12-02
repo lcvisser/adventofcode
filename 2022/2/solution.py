5aa6c5d0a9f0f29cd29141b9c793b48ca608bea7
Visser, Ludo <ludo.visser@cgi.com>
2022-12-02 10:23:50 +0100

    Day 2 part 1

--- 2022/2/solution.py
+++ 2022/2/solution.py
@@ -5,5 +5,38 @@ input_file = sys.argv[1]
 with open(input_file) as f:
     data = f.read()
 
+# A = rock, B = paper, C = scissors
+# X = rock, Y = paper, Z = scissors
+
+score_for_shape = {'X': 1, 'Y': 2, 'Z': 3}
+
+def outcome(my_shape, opp_shape):
+    played = (my_shape, opp_shape)
+    if played in [('X', 'C'), ('Y', 'A'), ('Z', 'B')]:
+        # Win
+        points = 6
+    elif played in [('X', 'A'), ('Y', 'B'), ('Z', 'C')]:
+        # Draw
+        points = 3
+    else:
+        # Loss
+        points = 0
+
+    return points
+
+
 # Parse data
 lines = data.strip().split('\n')
+
+total_score = 0
+for line in lines:
+    if line.strip() == '':
+        continue
+
+    opp_shape, my_shape = line.strip().split(' ')
+    shape_score = score_for_shape[my_shape]
+    play_outcome = outcome(my_shape, opp_shape)
+    total_score += shape_score + play_outcome
+
+# Part 1:
+print(f"Total score: {total_score}")
