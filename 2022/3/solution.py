6535a4be2076269620c05a9d4ca129a41a60fa41
lcvisser <ludo.visser@gmail.com>
2022-12-04 13:17:59 +0100

    Day 3 part 2

--- 2022/3/solution.py
+++ 2022/3/solution.py
@@ -8,20 +8,8 @@ with open(input_file) as f:
 # Parse data
 lines = data.strip().split('\n')
 
-# Part 1: sum of priorities
-total_priority = 0
-for line in lines:
-    if line.strip() == '':
-        continue
-
-    n = len(line)
-    assert(n % 2 == 0)
-
-    compartment1, compartment2 = set(line[0:n//2]), set(line[n//2:])
-    in_both = compartment1 & compartment2
-    assert(len(in_both) == 1)
-
-    priority = ord(in_both.pop())
+def char_to_priority(c):
+    priority = ord(c)
     if 65 <= priority <= 90:
         priority -= 38
     elif 97 <= priority <= 122:
@@ -29,6 +17,35 @@ for line in lines:
     else:
         raise ValueError(priority)
 
+    return priority
+
+
+# Part 1: sum of priorities
+total_priority = 0
+for line in lines:
+    n = len(line)
+    assert(n % 2 == 0)
+
+    compartment1, compartment2 = set(line[0:n//2]), set(line[n//2:])
+    in_both = compartment1 & compartment2
+    assert(len(in_both) == 1)
+
+    priority = char_to_priority(in_both.pop())
     total_priority += priority
 
 print(f"Sum of priorities: {total_priority}")
+
+# Part 2: sum of badge priorities
+assert(len(lines) % 3 == 0)
+i = 0
+total_badge_priority = 0
+while i < len(lines):
+    group1, group2, group3 = [set(g) for g in lines[i:i+3]]
+    badge = group1 & group2 & group3
+    assert(len(badge) == 1)
+
+    priority = char_to_priority(badge.pop())
+    total_badge_priority += priority
+    i += 3;
+
+print(f"Sum of badge priorities: {total_badge_priority}")
