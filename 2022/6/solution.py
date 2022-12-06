e0deac7f1a61fa9fb191a65104834a0709e6a871
Visser, Ludo <ludo.visser@cgi.com>
2022-12-06 09:16:04 +0100

    Day 6 part 2

--- 2022/6/solution.py
+++ 2022/6/solution.py
@@ -5,12 +5,20 @@ input_file = sys.argv[1]
 with open(input_file) as f:
     data = f.read()
 
-# Part 1: find unique preamble
-mark = -1;
-for i in range(len(data) - 4):
-    preamble = set(data[i:i + 4])
-    if len(preamble) == 4:
-        mark = i + 4
-        break
+def find_marker(data, length):
+    mark = -1;
+    for i in range(len(data) - length):
+        preamble = set(data[i:i + length])
+        if len(preamble) == length:
+            mark = i + length
+            break
 
+    return mark
+
+# Part 1: find preamble of length 4
+mark = find_marker(data, 4)
 print(f"First occurence of unique preamble: {mark}")
+
+# Part 2: find message of length 14
+mark = find_marker(data, 14)
+print(f"First occurence of unique message: {mark}")
