f723336699cc79980e79e45a9acfa6abc6e49447
Visser, Ludo <ludo.visser@cgi.com>
2022-12-09 10:57:46 +0100

    Day 9 part 2

--- 2022/9/solution.py
+++ 2022/9/solution.py
@@ -6,64 +6,64 @@ input_file = sys.argv[1]
 with open(input_file) as f:
     data = f.read()
 
-# data = """
-# R 4
-# U 4
-# L 3
-# D 1
-# R 4
-# D 1
-# L 5
-# R 2
-# """
-
 # Parse data
 lines = data.strip().split('\n')
 
-# Position x, y for head and tail
-posH = [0, 0]
-posT = [0, 0]
-tail_visited = set()
-for line in lines:
-    direction, distance = line.split()
-    distance = int(distance)
 
-    for i in range(distance):
-        # Move head
-        # print(f"H={posH}->", end='')
-        if direction == 'L':
-            posH[0] -= 1
-        elif direction == 'R':
-            posH[0] += 1
-        elif direction == 'U':
-            posH[1] += 1
-        elif direction == 'D':
-            posH[1] -= 1
-        # print(f"{posH}")
-
-        # Move tail if needed
-        # print(f"T={posT}->", end='')
-        distHT = math.dist(posH, posT)
-        if 0 <= distHT <= math.sqrt(2):
-            # H is less than one block away from T, so nothing to do
-            pass
-        elif math.sqrt(2) < distHT <= 2:
-            # H is 2 blocks away, either exactly horizontally or vertically, let T trail
-            if posT[0] == posH[0]:
-                posT[1] += int(math.copysign(1, posH[1] - posT[1]))
-            elif posT[1] == posH[1]:
-                posT[0] += int(math.copysign(1, posH[0] - posT[0]))
-            else:
-                ValueError("huh?")
-        else:
-            # Diagonal move to catch up
-            posT[0] += int(math.copysign(1, posH[0] - posT[0]))
+def follow(posH, posT):
+    # Move tail if needed
+    distHT = math.dist(posH, posT)
+    if 0 <= distHT <= math.sqrt(2):
+        # H is less than one block away from T, so nothing to do
+        pass
+    elif math.sqrt(2) < distHT <= 2:
+        # H is 2 blocks away, either exactly horizontally or vertically, let T trail
+        if posT[0] == posH[0]:
             posT[1] += int(math.copysign(1, posH[1] - posT[1]))
-        # print(f"{posT}")
-        # print()
+        elif posT[1] == posH[1]:
+            posT[0] += int(math.copysign(1, posH[0] - posT[0]))
+        else:
+            ValueError("huh?")
+    else:
+        # Diagonal move to catch up
+        posT[0] += int(math.copysign(1, posH[0] - posT[0]))
+        posT[1] += int(math.copysign(1, posH[1] - posT[1]))
 
-        # Add visited position
-        tail_visited.add(str(posT))
+    return posT
+
+
+def process(lines, N_knots):
+    # Position x, y for each knot
+    knots = [[0, 0] for _ in range(N_knots)]
+    tail_visited = set()
+
+    for line in lines:
+        direction, distance = line.split()
+        distance = int(distance)
+
+        for i in range(distance):
+            # Move head
+            if direction == 'L':
+                knots[0][0] -= 1
+            elif direction == 'R':
+                knots[0][0] += 1
+            elif direction == 'U':
+                knots[0][1] += 1
+            elif direction == 'D':
+                knots[0][1] -= 1
+
+            # Move other knots
+            for i in range(1, N_knots):
+                knots[i] = follow(knots[i - 1], knots[i])
+
+            # Add visited tail position
+            tail_visited.add(str(knots[-1]))
+
+    return len(tail_visited)
+
 
 # Part 1: number of unique positions visited by tail
-print(f"Unique number of places visited by tail: {len(tail_visited)}")
+print(f"Unique number of places visited by tail: {process(lines, 2)}")
+
+# Part 2: number of unique positions visited by 10th knot
+print(f"Unique number of places visited by tail: {process(lines, 10)}")
