e49167d2547240a27adc1f5fbf0eaf40c1816a56
lcvisser <ludo.visser@gmail.com>
2022-12-15 20:31:18 +0100

    Day 15 part 2

--- 2022/15/solution.py
+++ 2022/15/solution.py
@@ -1,31 +1,56 @@
 import sys
+import heapq
 
 # Read data
 input_file = sys.argv[1]
 with open(input_file) as f:
     data = f.read()
 
-data_example = """
-Sensor at x=2, y=18: closest beacon is at x=-2, y=15
-Sensor at x=9, y=16: closest beacon is at x=10, y=16
-Sensor at x=13, y=2: closest beacon is at x=15, y=3
-Sensor at x=12, y=14: closest beacon is at x=10, y=16
-Sensor at x=10, y=20: closest beacon is at x=10, y=16
-Sensor at x=14, y=17: closest beacon is at x=10, y=16
-Sensor at x=8, y=7: closest beacon is at x=2, y=10
-Sensor at x=2, y=0: closest beacon is at x=2, y=10
-Sensor at x=0, y=11: closest beacon is at x=2, y=10
-Sensor at x=20, y=14: closest beacon is at x=25, y=17
-Sensor at x=17, y=20: closest beacon is at x=21, y=22
-Sensor at x=16, y=7: closest beacon is at x=15, y=3
-Sensor at x=14, y=3: closest beacon is at x=15, y=3
-Sensor at x=20, y=1: closest beacon is at x=15, y=3
-"""
+target_row = 2000000
+
+
+# data = """
+# Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+# Sensor at x=9, y=16: closest beacon is at x=10, y=16
+# Sensor at x=13, y=2: closest beacon is at x=15, y=3
+# Sensor at x=12, y=14: closest beacon is at x=10, y=16
+# Sensor at x=10, y=20: closest beacon is at x=10, y=16
+# Sensor at x=14, y=17: closest beacon is at x=10, y=16
+# Sensor at x=8, y=7: closest beacon is at x=2, y=10
+# Sensor at x=2, y=0: closest beacon is at x=2, y=10
+# Sensor at x=0, y=11: closest beacon is at x=2, y=10
+# Sensor at x=20, y=14: closest beacon is at x=25, y=17
+# Sensor at x=17, y=20: closest beacon is at x=21, y=22
+# Sensor at x=16, y=7: closest beacon is at x=15, y=3
+# Sensor at x=14, y=3: closest beacon is at x=15, y=3
+# Sensor at x=20, y=1: closest beacon is at x=15, y=3
+# """
+# target_row = 10
+
+class Sensor:
+    def __init__(self, x, y, d):
+        self.x = x
+        self.y = y
+        self.distance_to_beacon = d
+
+        self.coverage_firstrow = self.y - d
+        self.coverage_lastrow = self.y + d
+
+    def has_coverage(self, row):
+        return self.coverage_firstrow <= row <= self.coverage_lastrow
+
+    def coverage(self, row):
+        if self.has_coverage(row):
+            w = self.distance_to_beacon - abs(self.y - row)
+            return self.x - w, self.x + w
+        else:
+            return None
+
 
 # Parse data
 sensor_coverage_at_target_row = []
+sensors = []
 beacons = []
-target_row = 2000000  # 10
 min_x = 0
 max_x = 0
 for line in data.strip().split('\n'):
@@ -42,22 +67,21 @@ for line in data.strip().split('\n'):
     max_x = max(max_x, beacon_pos[0])
 
     # Keep track of beacons on the target row
-    if beacon_pos[1] == target_row:
-        beacons.append(beacon_pos[0])
+    beacons.append(beacon_pos)
+
+    # Configure the sensor
+    dist_to_nearest_beacon = abs(beacon_pos[0] - sensor_pos[0]) + abs(beacon_pos[1] - sensor_pos[1])
+    sensor = Sensor(*sensor_pos, dist_to_nearest_beacon)
+    sensors.append(sensor)
 
     # Determine the maximum coverage of this sensor in x-direction
-    dist_to_nearest_beacon = abs(beacon_pos[0] - sensor_pos[0]) + abs(beacon_pos[1] - sensor_pos[1])
-    coverage_min_x = sensor_pos[0] - dist_to_nearest_beacon
-    coverage_max_x = sensor_pos[0] + dist_to_nearest_beacon
+    coverage_min_x, coverage_max_x = sensor.coverage(sensor_pos[1])
     min_x = min(min_x, coverage_min_x)
     max_x = max(max_x, coverage_max_x)
 
     # Determine coverage at target row
-    startrow_range = sensor_pos[1] - dist_to_nearest_beacon
-    endrow_range = sensor_pos[1] + dist_to_nearest_beacon
-    if startrow_range <= target_row <= endrow_range:
-        w = dist_to_nearest_beacon - abs(sensor_pos[1] - target_row)
-        coverage = (sensor_pos[0] - w, sensor_pos[0] + w)
+    coverage = sensor.coverage(target_row)
+    if coverage is not None:
         sensor_coverage_at_target_row.append(coverage)
         min_x = min(min_x, coverage[0])
         max_x = max(max_x, coverage[1])
@@ -69,7 +93,47 @@ for cs, ce in sensor_coverage_at_target_row:
     for i in range(cs, ce + 1):
         target_row_coverage[-min_x + i] = '#'
 for b in beacons:
-    target_row_coverage[-min_x + b] = 'B'
+    if b[1] == target_row:
+        target_row_coverage[-min_x + b[0]] = 'B'
 
 # Part 1: number of positions that cannot have a beacon
 print(f"Number of positions that cannot have a beacon at y={target_row}: {target_row_coverage.count('#')}")
+
+# Part 2: find the distress beacon
+max_dim = 4000000
+found = False
+for r in range(max_dim + 1):
+    total_coverage = []
+    for s in sensors:
+        sensor_coverage = s.coverage(r)
+        if sensor_coverage is None:
+            continue
+
+        # Sort sensor coverage by start and end using heapq
+        ss, se = sensor_coverage
+        ss = max(0, ss)
+        se = min(max_dim, se)
+        c = (ss, se)
+        heapq.heappush(total_coverage, c)
+
+    # Determine total coverage by popping from heap
+    cs = 0
+    ce = 1
+    while total_coverage and not found:
+        ss, se = heapq.heappop(total_coverage)
+        if ss > ce + 1:
+            # Gap in coverage, beacon found!
+            hidden_x = ce + 1
+            hidden_y = r
+            found = True
+        else:
+            # Adjacent or covering; merge
+            if se > ce:
+                ce = se
+
+    if found:
+        break
+
+# Tuning frequency of hidden beacon
+frequency = hidden_x * max_dim + hidden_y
+print(f"Hidden beacon at x={hidden_x}, y={hidden_y}; tuning frequency: {frequency}")
