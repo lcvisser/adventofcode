d9984131a3203aa2234e4cb414d0597b04acc399
Visser, Ludo <ludo.visser@cgi.com>
2022-12-16 23:05:38 +0100

    Day 16 part 1

--- 2022/16/solution.py
+++ 2022/16/solution.py
@@ -1,15 +1,13 @@
 import sys
-import dataclasses
-import heapq
-import typing
 import re
+import typing
 
 # Read data
 input_file = sys.argv[1]
 with open(input_file) as f:
     data = f.read()
 
-data = """
+data_example = """
 Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
 Valve BB has flow rate=13; tunnels lead to valves CC, AA
 Valve CC has flow rate=2; tunnels lead to valves DD, BB
@@ -23,30 +21,21 @@ Valve JJ has flow rate=21; tunnel leads to valve II
 """
 
 
-@dataclasses.dataclass
-class Valve:
+class Valve(typing.NamedTuple):
     name: str
-    rate: int
+    flowrate: int
     connected_to: typing.List
 
-    def __init__(self, name, rate, connected_to):
-        self.name = name
-        self.rate = rate
-        self.connected_to = connected_to
-        self.open = False
 
-
+# Parse data
 descriptor = re.compile(r"^Valve ([A-Z]{2}) has flow rate=([0-9]+); tunnels? leads? to valves? ([A-Z\ ,]+)$")
-
 valves = {}
 for line in data.strip().split('\n'):
     p = descriptor.match(line).groups()
     v = Valve(p[0], int(p[1]), list(p[2].split(', ')))
     valves[v.name] = v
-    print(v)
 
-
-# Breadth-first search to find shortest path
+# Helper function for a breadth-first search to find the shortest path between two valves
 def bfs(src, dst):
     to_visit = [src]
     visited = set()
@@ -73,35 +62,34 @@ def bfs(src, dst):
 
     return d
 
+# Determine shortest distance matrix
 dist_matrix = {src: {dst: bfs(src, dst) for dst in valves.keys()} for src in valves.keys()}
-for r, v in dist_matrix.items():
-    print(r, v)
 
-curr = "AA"
-total_pressure_release = 0
-t = 0
-t_end = 30
-while t <= t_end:
-    print(t, curr)
-    options_release = []
-    for v in valves.values():
-        if v.open:
-            continue
+# Do a depth-first search for all possible paths within 30 minutes
+to_visit = [(0, "AA", [], 0)]  # time, current valve, route so far, pressure release so far
+max_p = 0
+while to_visit:
+    t, v, route, p = to_visit.pop(0)
 
-        time_to_get_there = dist_matrix[curr][v.name] + 1  # plus one minute to open the valve
-        if time_to_get_there > t_end - t:
-            continue
+    # Find all reachable (t < 30 min) and useful (flow rate > 0) valves
+    possible_dest = [
+        (w, dt) for w, dt in dist_matrix[v].items() if w != v and
+                                                       w not in route and
+                                                       t + dt < 30
+                                                       and valves[w].flowrate != 0
+    ]
 
-        p = (t_end - t - time_to_get_there) * valves[v.name].rate
-        options_release.append((p, v.name, time_to_get_there))
-
-    if options_release:
-        dp, v, dt = max(options_release, key=lambda x: x[0])
-        valves[v].open = True
-        total_pressure_release += dp
-        t += dt
-        curr = v
+    # Determine if we have somewhere to go still
+    if possible_dest:
+        for w, dt in possible_dest:
+            travel_plus_open_time = dt + 1  # 1 minute to open
+            arrival_time = t + travel_plus_open_time
+            pressure_release = p + (30 - arrival_time) * valves[w].flowrate
+            to_visit.insert(0, (arrival_time, w, route + [w], pressure_release))
     else:
-        t += 1
+        # End of path; keep track of maximum pressure release
+        if p > max_p:
+            max_p = p
 
-print(total_pressure_release)
+# Part 1: maximum pressure release
+print(f"Maximum pressure release: {max_p}")
