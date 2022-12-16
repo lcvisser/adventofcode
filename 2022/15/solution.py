c84afbace9f55d304c56b7894873b4f31f413982
Visser, Ludo <ludo.visser@cgi.com>
2022-12-16 16:42:02 +0100

    Day 15 part 2

--- 2022/15/solution.py
+++ 2022/15/solution.py
@@ -6,26 +6,6 @@ input_file = sys.argv[1]
 with open(input_file) as f:
     data = f.read()
 
-target_row = 2000000
-
-
-# data = """
-# Sensor at x=2, y=18: closest beacon is at x=-2, y=15
-# Sensor at x=9, y=16: closest beacon is at x=10, y=16
-# Sensor at x=13, y=2: closest beacon is at x=15, y=3
-# Sensor at x=12, y=14: closest beacon is at x=10, y=16
-# Sensor at x=10, y=20: closest beacon is at x=10, y=16
-# Sensor at x=14, y=17: closest beacon is at x=10, y=16
-# Sensor at x=8, y=7: closest beacon is at x=2, y=10
-# Sensor at x=2, y=0: closest beacon is at x=2, y=10
-# Sensor at x=0, y=11: closest beacon is at x=2, y=10
-# Sensor at x=20, y=14: closest beacon is at x=25, y=17
-# Sensor at x=17, y=20: closest beacon is at x=21, y=22
-# Sensor at x=16, y=7: closest beacon is at x=15, y=3
-# Sensor at x=14, y=3: closest beacon is at x=15, y=3
-# Sensor at x=20, y=1: closest beacon is at x=15, y=3
-# """
-# target_row = 10
 
 class Sensor:
     def __init__(self, x, y, d):
@@ -48,6 +28,7 @@ class Sensor:
 
 
 # Parse data
+target_row = 2000000
 sensor_coverage_at_target_row = []
 sensors = []
 beacons = []
