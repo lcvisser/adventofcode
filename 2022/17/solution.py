9386fca60882819447910add9f11e7efc16780fc
lcvisser <ludo.visser@gmail.com>
2022-12-18 22:58:33 +0100

    Day 17 part 2 (finally)

--- 2022/17/solution.py
+++ 2022/17/solution.py
@@ -1,26 +1,25 @@
 import sys
-import collections
 import functools
 
 # Read data
-# input_file = sys.argv[1]
-# with open(input_file) as f:
-#     data = f.read()
+input_file = sys.argv[1]
+with open(input_file) as f:
+    data = f.read()
 
-data = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
+data_ex = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
 
 # Parse data
-jets = collections.deque(1 if j == '>' else -1 for j in data.strip())
+jets = tuple(1 if j == '>' else -1 for j in data.strip())
 num_jets = len(jets)
 
 # Rocks, defined bottom to top
-rocks = collections.deque([
+rocks = (
     (0xF << 1, ),  # -
     (0x2 << 2, 0x7 << 2, 0x2 << 2),  # +
     (0x7 << 2, 0x1 << 2, 0x1 << 2),  # mirrored L
     (0x1 << 4, 0x1 << 4, 0x1 << 4, 0x1 << 4),  # |
     (0x3 << 3, 0x3 << 3)  # 2x2 cube
-])
+)
 num_rocks = len(rocks)
 
 
@@ -58,14 +57,50 @@ def step(jet_push, rock, cave_section):
 
 
 def drop(target_rock_count):
+    # Initialize
+    jet_index = 0
+    rock_index = 0
+
     # Cave as a stack, growing on the right as we go up
     cave = [0x7f]
 
+    # Keep a cache of cave/rock/jet combos to see if we're repeating
+    seen_combos = {}
+    extra_heigth = 0
+    cycle_found = False
+
+    # Start dropping rocks
     rock_count = 0
     while rock_count < target_rock_count:
+        # Determine the maximum fill for each column to see what we should save; if the whole width is filled, we know
+        # (almost for sure) that the cave won't change below
+        found = 0
+        h = len(cave)
+        depth = 0
+        while found != 0x7f and h > 0:
+            h -= 1
+            depth += 1
+            found = found | cave[h]
+
+        # Determien if we are repeating
+        if not cycle_found:
+            combo = (tuple(cave[-depth:]), rock_index, jet_index)
+            if combo in seen_combos.keys():
+                # Determine repeate cycle parameters
+                cycle_offset = list(seen_combos.keys()).index(combo)
+                cycle_length = len(seen_combos.keys()) - cycle_offset
+                cycle_growth = len(cave) - 1 - seen_combos[combo]
+                extra_cycles = (target_rock_count - rock_count) // cycle_length
+
+                # Fast-forward
+                extra_heigth = cycle_growth * extra_cycles
+                rock_count += extra_cycles * cycle_length
+                cycle_found = True
+            else:
+                seen_combos[combo] = len(cave) - 1
+
         # Get rock
-        rock = rocks[0]
-        rocks.rotate(-1)
+        rock = rocks[rock_index]
 
         # Add rows for the rock and empty space
         rock_size = len(rock)
@@ -73,8 +108,8 @@ def drop(target_rock_count):
             cave.append(0)
 
         for i in reversed(range(1, len(cave) - rock_size + 1)):
-            jet_push = jets[0]
-            jets.rotate(-1)
+            jet_push = jets[jet_index]
+            jet_index = (jet_index + 1) % num_jets
 
             cave_section = tuple(cave[i - 1:i + rock_size])
             rock, stopped, section_update = step(jet_push, rock, cave_section)
@@ -89,21 +124,15 @@ def drop(target_rock_count):
             cave.pop()
 
         rock_count += 1
+        rock_index = (rock_index + 1) % num_rocks
 
-    return len(cave) - 1
+    return len(cave) - 1 + extra_heigth
 
 # Part 1: drop 2022 rocks
 h = drop(2022)
 print(f"Rock height after 2022 rocks: {h}")
-step.cache_clear()
 
 # Part 2: drop a lot of rocks
 desired_rock_count = 1000000000000
-min_cycle = 241 * num_jets * num_rocks
-full_cycles, partial_cycles = divmod(desired_rock_count, min_cycle)
-h = drop(min_cycle) * full_cycles + drop(partial_cycles)
-print(h)
-print(step.cache_info())
-# 1514285714288 <--
-# 1514285714287
-# 1399970062786
+h = drop(desired_rock_count)
+print(f"Rock height after {desired_rock_count} rocks: {h}")
