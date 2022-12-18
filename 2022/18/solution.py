e551ec32194d1c8f7d0e6e65a5e23802317cf8a8
lcvisser <ludo.visser@gmail.com>
2022-12-18 18:27:36 +0100

    Day 18 part 2: works on example, not on real data

--- 2022/18/solution.py
+++ 2022/18/solution.py
@@ -5,7 +5,7 @@ input_file = sys.argv[1]
 with open(input_file) as f:
     data = f.read()
 
-dataex = """
+data = """
 2,2,2
 1,2,2
 3,2,2
@@ -25,9 +25,9 @@ dataex = """
 XY = {}
 XZ = {}
 YZ = {}
-for cube in data.strip().split('\n'):
-    x, y, z = [int(n) for n in cube.split(',')]
 
+# Helper function to update plane views
+def add_to_planes(x, y, z):
     xy = (x, y)
     if xy not in XY.keys():
         XY[xy] = []
@@ -46,10 +46,16 @@ for cube in data.strip().split('\n'):
     if x not in YZ[yz]:
         YZ[yz].append(x)
 
-# Helper function to count faces per plane
+for cube in data.strip().split('\n'):
+    x, y, z = [int(n) for n in cube.split(',')]
+    add_to_planes(x, y, z)
+
+
+# Helper function to count faces per plane view
 def count_faces(plane):
+    potential_pockets = []
     face_count = 0
-    for col in plane.values():
+    for coord, col in plane.items():
         prev_h = None
         for h in sorted(col):
             if prev_h is None:
@@ -57,17 +63,34 @@ def count_faces(plane):
                 prev_h = h
             else:
                 if h > prev_h + 1:
+                    # Pocket
                     face_count += 2  # close previous stack, start new stack
+                    for i in range(prev_h + 1, h):
+                        potential_pockets.append((coord, i))
 
             prev_h = h
 
         col_face_count += 1  # final face
         face_count += col_face_count
 
-    return face_count
+    return face_count, potential_pockets
 
 # Part 1: surface area
-xy_faces = count_faces(XY)
-xz_faces = count_faces(XZ)
-yz_faces = count_faces(YZ)
+xy_faces, pockets_xy = count_faces(XY)
+xz_faces, pockets_xz = count_faces(XZ)
+yz_faces, pockets_yz = count_faces(YZ)
 print(f"Surface area: {xy_faces + xz_faces + yz_faces}")
+
+# Part 2: remove pockets
+potential_pockets1 = set((x, y, z) for (x, y), z in pockets_xy)
+potential_pockets2 = set((x, y, z) for (x, z), y in pockets_xz)
+potential_pockets3 = set((x, y, z) for (y, z), x in pockets_yz)
+pockets = potential_pockets1.intersection(potential_pockets2).intersection(potential_pockets3)
+# print(pockets)
+for x, y, z in pockets:
+    add_to_planes(x, y, z)
+
+xy_faces, _ = count_faces(XY)
+xz_faces, _ = count_faces(XZ)
+yz_faces, _ = count_faces(YZ)
+print(f"Surface area without pockets: {xy_faces + xz_faces + yz_faces}")
