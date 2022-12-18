ea56bb3374392cbb4cb433713f58f5b29094b73e
lcvisser <ludo.visser@gmail.com>
2022-12-18 18:06:38 +0100

    Day 17 part 2 refactoring

--- 2022/17/solution.py
+++ 2022/17/solution.py
@@ -1,5 +1,6 @@
 import sys
 import collections
+import functools
 
 # Read data
 # input_file = sys.argv[1]
@@ -31,14 +32,39 @@ def push(r, j):
         return r << abs(j)
 
 
+@functools.cache
+def step(jet_push, rock, cave_section):
+    # Check collision with wall or other rocks
+    for x, r in enumerate(rock):
+        lb = 0x80 | cave_section[1 + x]
+        rb = cave_section[1 + x]
+        pushed_r = push(r, jet_push)
+        blocked_left = (pushed_r & lb > 0) and jet_push == -1
+        blocked_right = (pushed_r & rb > 0 or r & 0x1) and jet_push == 1
+        if blocked_left or blocked_right:
+            break
+    else:
+        rock = tuple(push(r, jet_push) for r in rock)
+
+    # Check if we hit anywhere on the row below
+    if any(r & cave_section[x] for x, r in enumerate(rock)):
+        section_update = tuple(cave_section[1 + x] | r for x, r in enumerate(rock))
+        stopped = True
+    else:
+        section_update = None
+        stopped = False
+
+    return rock, stopped, section_update
+
+
 def drop(target_rock_count):
     # Cave as a stack, growing on the right as we go up
-    cave = collections.deque([0x7f])
+    cave = [0x7f]
 
     rock_count = 0
     while rock_count < target_rock_count:
         # Get rock
-        rock = list(rocks[0])
+        rock = rocks[0]
         rocks.rotate(-1)
 
         # Add rows for the rock and empty space
@@ -50,31 +76,17 @@ def drop(target_rock_count):
             jet_push = jets[0]
             jets.rotate(-1)
 
-            # Check collision with wall or other rocks
-            for x, r in enumerate(rock):
-                lb = 0x80 | cave[i + x]
-                rb = cave[i + x]
-                pushed_r = push(r, jet_push)
-                blocked_left = (pushed_r & lb > 0) and jet_push == -1
-                blocked_right = (pushed_r & rb > 0 or r & 0x1) and jet_push == 1
-                if blocked_left or blocked_right:
-                    break
-            else:
-                rock = [push(r, jet_push) for r in rock]
-
-            # Check if we hit the bottom
-            if any(r & cave[i + x - 1] for x, r in enumerate(rock)):
-                for x, r in enumerate(rock):
-                    cave[i + x] = cave[i + x] | r
+            cave_section = tuple(cave[i - 1:i + rock_size])
+            rock, stopped, section_update = step(jet_push, rock, cave_section)
+            if stopped:
+                for x, r in enumerate(section_update):
+                    cave[i + x] = r
 
                 break
 
         # Remove the empty layers of cave
         while cave[-1] == 0:
             cave.pop()
-
-        if cave[-1] == 127:
-            print(rock_count)
 
         rock_count += 1
 
@@ -83,11 +95,15 @@ def drop(target_rock_count):
 # Part 1: drop 2022 rocks
 h = drop(2022)
 print(f"Rock height after 2022 rocks: {h}")
+step.cache_clear()
 
 # Part 2: drop a lot of rocks
 desired_rock_count = 1000000000000
-min_cycle = num_jets * num_rocks
-drop(1000000000000)
-
+min_cycle = 241 * num_jets * num_rocks
+full_cycles, partial_cycles = divmod(desired_rock_count, min_cycle)
+h = drop(min_cycle) * full_cycles + drop(partial_cycles)
+print(h)
+print(step.cache_info())
 # 1514285714288 <--
 # 1514285714287
+# 1399970062786
