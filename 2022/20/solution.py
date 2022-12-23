07a2dc145ee2b5f642cdca2b93112fdb49c7f76b
lcvisser <ludo.visser@gmail.com>
2022-12-23 01:03:36 +0100

    Day 20 part 1 (I suck at counting, apparently)

--- 2022/20/solution.py
+++ 2022/20/solution.py
@@ -6,7 +6,7 @@ with open(input_file) as f:
     data = f.read()
 
 
-data = """
+data_ex = """
 1
 2
 -3
@@ -18,41 +18,47 @@ data = """
 
 # Parse data
 encrypted_file = [(i, int(x)) for i, x in enumerate(data.strip().split('\n'))]
+decrypted_file = encrypted_file.copy()
 num = len(encrypted_file)
 
-decrypted_file = encrypted_file.copy()
-# print(0, [v for _, v in decrypted_file])
+# Decrypt
 for i in range(num):
-
+    # Find the original item at this location
     item = encrypted_file[i]
     _, value = item
     if value == 0:
         continue
 
+    # Determine where it is now and how much it should move (abs(v) // v gives the sign, abs(v) % (n - 1) the number of
+    # places we need to move; the -1 is because n - 1 moves in the circular list brings us back to the orginal
+    # position)
     curr = decrypted_file.index(item)
-    goto = decrypted_file[(curr + value) % num]
-    decrypted_file.pop(curr)
-    move_index = decrypted_file.index(goto)
+    to_move = (abs(value) // value) * (abs(value) % (num - 1))
 
-    if value > 0:
-        decrypted_file.insert(move_index + 1, item)
+    # Determine which index we need to insert after
+    if to_move > 0:
+        index_to_insert_after = (curr + to_move) % num
     else:
-        if move_index == 0:
-            decrypted_file.append(item)
-        else:
-            decrypted_file.insert(move_index, item)
+        index_to_insert_after = (curr + to_move) % num - 1  # if we move backwards, we need to go one further
 
+    # Track the item we want to insert after and remove the item being moved
+    item_to_insert_after = decrypted_file[index_to_insert_after]
+    decrypted_file.remove(item)
 
-    # print(i + 1, [v for _, v in decrypted_file])
+    # Find the target item back and insert after it
+    goto = decrypted_file.index(item_to_insert_after)
+    decrypted_file.insert(goto + 1, item)
 
-# print('-', [v for _, v in decrypted_file])
+# Find the location of the zero
 zeroindex = None
 for i, (_, v) in enumerate(decrypted_file):
     if v == 0:
         zeroindex = i
         break
 
+# Part 1: coordinate after mixing
 item1000 = decrypted_file[(zeroindex + 1000) % num]
 item2000 = decrypted_file[(zeroindex + 2000) % num]
 item3000 = decrypted_file[(zeroindex + 3000) % num]
-print(sum([v for _, v in [item1000, item2000, item3000]]))
+coordinate = sum([v for _, v in [item1000, item2000, item3000]])
+print(f"Coordinate: {coordinate}")
