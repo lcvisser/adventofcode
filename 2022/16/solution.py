e10b6c63b9bee80960e5013f872ed674ccced1c1
lcvisser <ludo.visser@gmail.com>
2023-01-14 12:52:00 +0100

    Day 16 part 2 (finally, with some help)

--- 2022/16/solution.py
+++ 2022/16/solution.py
@@ -4,22 +4,10 @@ import re
 import typing
 
 # Read data
-#input_file = sys.argv[1]
-#with open(input_file) as f:
-#    data = f.read()
+input_file = sys.argv[1]
+with open(input_file) as f:
+    data = f.read()
 
-data = """
-Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
-Valve BB has flow rate=13; tunnels lead to valves CC, AA
-Valve CC has flow rate=2; tunnels lead to valves DD, BB
-Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
-Valve EE has flow rate=3; tunnels lead to valves FF, DD
-Valve FF has flow rate=0; tunnels lead to valves EE, GG
-Valve GG has flow rate=0; tunnels lead to valves FF, HH
-Valve HH has flow rate=22; tunnel leads to valve GG
-Valve II has flow rate=0; tunnels lead to valves AA, JJ
-Valve JJ has flow rate=21; tunnel leads to valve II
-"""
 
 class Valve(typing.NamedTuple):
     name: str
@@ -65,66 +53,42 @@ def bfs(src, dst):
 # Determine shortest distance matrix
 dist_matrix = {src: {dst: bfs(src, dst) for dst in valves.keys()} for src in valves.keys()}
 
-# Do a depth-first search for all possible paths within 30 minutes
-t_end = 30
-max_p1 = 0
-to_process = [(0, "AA", 0, [])]  # time, current valve, pressure release so faf, visited
-while to_process:
-    t, v, p, visited = to_process.pop(0)
+# Helper function to find all routes through the network and keep track of pressure released
+def find_paths(t_end, min_pressure=0):
+    # Do a depth-first search for all possible paths within 30 minutes
+    route_pressure_release = {}
+    to_process = [(0, "AA", 0, [])]  # time, current valve, pressure release so far, visited
+    while to_process:
+        t, v, p, visited = to_process.pop(0)
+        if p > min_pressure:
+            route_pressure_release[tuple(visited)] = p
 
-    # Keep track of maximum pressure release
-    max_p1 = max(max_p1, p)
+        # Find all reachable in time and useful (flow rate > 0) valves
+        possible_dest = [
+            (t + dt + 1, w, p + (t_end - (t + dt + 1)) * valves[w].flowrate)
+                for w, dt in dist_matrix[v].items() if w != v and
+                                                    w not in visited and
+                                                    t + dt + 1 < t_end and
+                                                    valves[w].flowrate > 0
+        ]
 
-    # Find all reachable (t < 30 min) and useful (flow rate > 0) valves
-    possible_dest = [
-        (t + dt + 1, w, p + (t_end - (t + dt + 1)) * valves[w].flowrate)
-            for w, dt in dist_matrix[v].items() if w != v and
-                                                   w not in visited and
-                                                   t + dt + 1 < t_end and
-                                                   valves[w].flowrate > 0
-    ]
+        # Go to remaining destinations
+        for arrival_time, w, p in possible_dest:
+            to_process.insert(0, (arrival_time, w, p, visited + [w]))
 
-    # Go to remaning destinations
-    for arrival_time, w, p in possible_dest:
-        to_process.insert(0, (arrival_time, w, p, visited + [v]))
+    return route_pressure_release
 
 # Part 1: maximum pressure release working alone
+pressure_release30 = find_paths(30)
+max_p1 = max(pressure_release30.values())
 print(f"Maximum pressure release working alone: {max_p1}")
 
-# Do a depth-first search for all possible combinations of visits within 26 minutes
-t_end = 26
-max_p2 = 0
-to_process = [((0, "AA", 0), (0, "AA", 0), [])]  # time, current valve and pressure release for me and elephant; visited
-while to_process:
-    me, elephant, visited = to_process.pop(0)
-    mt, mloc, mp = me
-    et, eloc, ep = elephant
-
-    # Keep track of maximum pressure release
-    max_p2 = max(max_p2, mp + ep)
-
-    # Find all reachable (t < 26 min) and useful (flow rate > 0) valves for me
-    my_possible_dest = [
-        (mt + dt + 1, v, mp + (t_end - (mt + dt + 1)) * valves[v].flowrate)
-            for v, dt in dist_matrix[mloc].items() if v != mloc and
-                                                      v not in visited and
-                                                      mt + dt + 1 < t_end and
-                                                      valves[v].flowrate > 0
-    ]
-
-    # Find all reachable (t < 26 min) and useful (flow rate > 0) valves for the elephant
-    elephant_possible_dest = [
-        (et + dt + 1, w, ep + (t_end - (et + dt + 1)) * valves[w].flowrate)
-            for w, dt in dist_matrix[eloc].items() if w != eloc and
-                                                      w not in visited and
-                                                      et + dt + 1 < t_end and
-                                                      valves[w].flowrate > 0
-    ]
-
-    # Go to remaining destinations (all combinations thereof)
-    for d1, d2 in itertools.product(my_possible_dest, elephant_possible_dest):
-        if d1[1] != d2[1]:  # don't go to the same valve
-            to_process.insert(0, (d1, d2, visited + [d1[1], d2[1]]))
-
 # Part 2: maximum pressure release working with the elephant
+pressure_release26 = find_paths(26, max_p1 / 2)  # assume that together we do at least as good of a job as working alone
+max_p2 = 0
+for my_route, elephant_route in itertools.combinations(pressure_release26.keys(), 2):
+    if set(my_route).isdisjoint(set(elephant_route)):
+        p = pressure_release26[my_route] + pressure_release26[elephant_route]
+        max_p2 = max(max_p2, p)
+
 print(f"Maximum pressure release working together with the elephant: {max_p2}")
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-My attempts at solving the Advent Of Code puzzles (www.adventofcode.com). I'm not that good ;-)
+My attempts at solving the Advent Of Code puzzles (www.adventofcode.com).
 
 This is primarily a learning exercise for myself. The goal is to look back later and have learned better ways to solve
-a particular problem in the mean time.
+a particular problem in the mean time. When I get stuck, I might look up other people's solutions for inspiration (I
+especially like [Martijn Pieters' writeups](https://github.com/mjpieters/adventofcode)), but only after the event has
+finished. Any commits done after the end of the events are from me puzzling along a bit longer, probably with some help.
