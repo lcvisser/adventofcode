661e4650061ac3dc48f0f2a21ac880ed4067536c
lcvisser <ludo.visser@gmail.com>
2023-01-15 22:50:09 +0100

    Day 24 part 2

--- 2022/24/solution.py
+++ 2022/24/solution.py
@@ -1,21 +1,12 @@
 import sys
 import collections
 import functools
+import math
 
 # Read data
 input_file = sys.argv[1]
 with open(input_file) as f:
     data = f.read()
-
-dataex = """
-#.######
-#>>.<^<#
-#.<..<<#
-#>v.><>#
-#<^v^^>#
-######.#
-"""
-
 
 # Parse data
 valley_description = data.strip().split('\n')
@@ -60,6 +51,8 @@ for r in range(height):
         else:
             pass
 
+# Blizzard pattern repeats
+blizzard_cycle_time = (height - 2) * (width - 2) // math.gcd(height - 2, width - 2)
 
 # Position is (row, col)
 start = (0, valley_description[0].index('.'))
@@ -79,6 +72,8 @@ def get_neighbors(t, pos):
     possible_neighbors = [pos]  # waiting in place is an option too and can be faster!
     if pos == start:
         possible_neighbors.append((r + 1, c))
+    elif pos == dest:
+        possible_neighbors.append((r - 1, c))
     else:
         if r > 1:
             possible_neighbors.append((r - 1, c))
@@ -92,25 +87,44 @@ def get_neighbors(t, pos):
     if pos == (dest[0] - 1, dest[1]):
         possible_neighbors.append(dest)
 
+    if pos == (start[0] + 1, start[1]):
+        possible_neighbors.append(start)
+
     blizzard_positions = get_blizzard_positions(t)
     allowed_neighbors = [p for p in possible_neighbors if p not in blizzard_positions]
 
     return allowed_neighbors
 
 
-# Part 1: breadth-first search to find shortest route to exit
-to_process = collections.deque()
-state = (0, start, [start])
-to_process.append(state)
-visited = {(0, start)}
-while to_process:
-    t, v, route = to_process.popleft()
-    if v == dest:
-        print(f"Reached exit at t={t}")
-        break
+# Breadth-first search to find shortest route
+def find_path(begin, end, t0=0):
+    travel_time = float("inf")
+    state = (t0, begin, [begin])
+    visited = {(t0 % blizzard_cycle_time, begin)}
 
-    options = get_neighbors(t + 1, v)
-    for w in options:
-        if (t + 1, w) not in visited:
-            visited.add((t + 1, w))
-            to_process.append((t + 1, w, route + [w]))
+    to_process = collections.deque()
+    to_process.append(state)
+    while to_process:
+        t, v, route = to_process.popleft()
+        if v == end:
+            travel_time = t - t0
+            break
+
+        # Get state of the map considering cyclic nature of the blizzards
+        options = get_neighbors((t + 1) % blizzard_cycle_time, v)
+        for w in options:
+            if ((t + 1) % blizzard_cycle_time, w) not in visited:
+                # State is cyclic, but track actual time
+                visited.add(((t + 1) % blizzard_cycle_time, w))
+                to_process.append((t + 1, w, route + [w]))
+
+    return travel_time
+
+# Part 1: find the shortest travel time
+t1 = find_path(start, dest)
+print(f"Shortest travel time: {t1}")
+
+# Part 2: go back for the snacks
+t2 = find_path(dest, start, t0=t1)
+t3 = find_path(start, dest, t0=t1 + t2)
+print(f"Time to reach goal, go back, and go to goal again: {t1 + t2 + t3}")
