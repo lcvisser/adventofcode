f791854e52706cb8af8fdee38dc86d1c74759a3d
lcvisser <ludo.visser@gmail.com>
2023-01-15 19:49:58 +0100

    Day 19 part 2

--- 2022/19/solution.py
+++ 2022/19/solution.py
@@ -1,17 +1,13 @@
 import sys
 import collections
 import functools
+import math
 import re
 
 # Read data
 input_file = sys.argv[1]
 with open(input_file) as f:
     data = f.read()
-
-dataex = """
-Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
-Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
-"""
 
 # Parse data
 robot_descriptor = re.compile(r"Each ([a-z]+) robot costs ([0-9]+) ([a-z]+)(?: and ([0-9]+) ([a-z]+)(?: and )?)*\.")
@@ -30,23 +26,33 @@ for line in data.strip().split('\n'):
     blueprints[blueprint_id] = tuple(robot_costs)
 
 
+# Enumerated materials
+ORE = 0
+CLAY = 1
+OBSIDIAN = 2
+GEODE = 3
+
+
 # Helper function to determine if a robot can be built with the materials availble
 @functools.cache
 def can_be_built(have, need):
     return all(h >= n for h, n in zip(have, need))
 
+
 # Helper function to determine robot build options and impact on collected materials
 @functools.cache
 def determine_options(materials_collected, robots_available, blueprint):
-    # Step 0: determine build options (assume we build one robot at the time)
-    robot_build_options = [None] + [i for i, _ in enumerate(materials) if can_be_built(tuple(materials_collected), blueprint[i])]
+    # Step 0: determine build options (assume we build one robot at the time); not building is also an option
+    robot_build_options = [None] + [i for i in (ORE, CLAY, OBSIDIAN, GEODE) if can_be_built(tuple(materials_collected), blueprint[i])]
 
-    # Prioritize
-    for i in [0, 1, 2]:
+    # Prioritize: we can only build one robot per iteration, so if we need at most N material for a particular robot to
+    #  build, there is no point in having more than N robots collecting that material
+    for i in (ORE, CLAY, OBSIDIAN):
         if robots_available[i] >= max(c[i] for c in blueprint) and i in robot_build_options:
             robot_build_options.remove(i)
 
-    if 4 in robot_build_options or 3 in robot_build_options:
+    # If we can build geode robot, build it, don't consider not building
+    if GEODE in robot_build_options:
         robot_build_options.remove(None)
 
     # Process each option as a simulation step (prefer building over not building)
@@ -60,7 +66,7 @@ def determine_options(materials_collected, robots_available, blueprint):
                 opt_materials_collected[i] -= c
 
         # Step 2: collect materials
-        for i, _ in enumerate(materials):
+        for i in (ORE, CLAY, OBSIDIAN, GEODE):
             opt_materials_collected[i] += robots_available[i]
 
         # Step 3: build robots
@@ -74,8 +80,7 @@ def determine_options(materials_collected, robots_available, blueprint):
 
 
 # Function to evalute blueprint
-def evaluate_blueprint(blueprint):
-    t_end = 24
+def evaluate_blueprint(blueprint, t_end):
     max_geodes = 0
     initial_materials_collected = (0, 0, 0, 0)
     initial_robots_built = (1, 0, 0, 0)
@@ -84,6 +89,7 @@ def evaluate_blueprint(blueprint):
     to_process.appendleft((0, initial_materials_collected, initial_robots_built))
     seen = set()
     while to_process:
+        # Keep track of states already processed
         state = to_process.popleft()
         if state in seen:
             continue
@@ -92,17 +98,20 @@ def evaluate_blueprint(blueprint):
 
         t, materials_collected, robots_available = state
 
-        # Determine geodes collected at the end
+        # Determine number of geodes collected at the end
         if t == t_end:
-            if materials_collected[3] > max_geodes:
-                max_geodes = materials_collected[3]
-                print(max_geodes)
+            if materials_collected[GEODE] > max_geodes:
+                max_geodes = materials_collected[GEODE]
             continue
 
-        geodes_collected = materials_collected[3]
+        # With T minutes remaining, we can collect at most T * num_geode_robots geodes plus T * (T + 1) / 2 additional
+        # geodes if we were to build an additional geode robot every iteration (see triangle number); if this potential
+        # number of geodes is lower than the maximum already known, abort this path
+        geodes_collected = materials_collected[GEODE]
         time_remaining = t_end - t
-        potential_geodes_to_collect = robots_available[3] * time_remaining + time_remaining * (time_remaining + 1) // 2
+        potential_geodes_to_collect = time_remaining * robots_available[GEODE] + time_remaining * (time_remaining + 1) // 2
         if geodes_collected + potential_geodes_to_collect < max_geodes:
+            # We're not going to catch up, give up this branch
             continue
 
         # Determine next step options
@@ -114,14 +123,22 @@ def evaluate_blueprint(blueprint):
 
 
 # Part 1: sum of quality levels
-total = 0
+quality_levels = []
 for bpid, blueprint in blueprints.items():
-    determine_options.cache_clear()
-    q = evaluate_blueprint(blueprint)
-    print(can_be_built.cache_info())
-    print(determine_options.cache_info())
+    q = evaluate_blueprint(blueprint, 24)
+    quality = bpid * q
 
-    print(f"id={bpid}: q={q}")
-    total += bpid * q
+    print(f"Blueprint id={bpid}: {q} geodes collected (quality={quality})")
+    quality_levels.append(quality)
 
-print(f"Sum of quality levels: {total}")
+print(f"Sum of quality levels: {sum(quality_levels)}")
+
+# Part 2: total geodes
+geodes_collected = []
+for bpid in (1, 2, 3):
+    q = evaluate_blueprint(blueprints[bpid], 32)
+
+    print(f"Blueprint id={bpid}: {q} geodes collected")
+    geodes_collected.append(q)
+
+print(f"Product of geodes collected: {math.prod(geodes_collected)}")
