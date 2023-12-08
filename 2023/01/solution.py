eb70a1134065055b64b8b27509a21e5e397ff322
lcvisser <ludo.visser@gmail.com>
2023-12-08 10:44:04 +0100

    Day 1, part 2

--- 2023/01/solution.py
+++ 2023/01/solution.py
@@ -1,4 +1,3 @@
-
 import sys
 
 # Read data
@@ -7,10 +6,46 @@ with open(input_file) as f:
     data = f.read()
 
 # Parse data
-calibration_values = []
+calibration_values1 = []
 for line in data.strip().split('\n'):
     digits = [int(c) for c in line if c in "0123456789"]
-    calibration_values.append(10 * digits[0] + digits[-1])
+    calibration_values1.append(10 * digits[0] + digits[-1])
 
-# Part 1: sum of calibration values:
-print(f"Sum of calibration values: {sum(calibration_values)}")
+# Part 1: sum of calibration values
+print(f"Sum of calibration values: {sum(calibration_values1)}")
+
+
+text_to_numbers = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+
+calibration_values2 = []
+for line in data.strip().split('\n'):
+    digits = []
+    token = ""
+    for c in line:
+        if c in "0123456789":
+            digits.append(int(c))
+            token = ""
+        else:
+            token += c
+            found = [token.find(k) >= 0 for k in text_to_numbers.keys()]
+            if any(found):
+                key_index = found.index(True)
+                number = list(text_to_numbers.values())[key_index]
+
+                digits.append(number)
+                token = c  # numbers are allowed to overlap (!)
+
+    calibration_values2.append(10 * digits[0] + digits[-1])
+
+# Part 2: sum of calibration values
+print(f"Sum of calibration values: {sum(calibration_values2)}")
