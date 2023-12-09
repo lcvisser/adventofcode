74ca6ca2dcc365228cc8e6a135946634e2ddff10
Ludo Visser <ludo.visser@gmail.com>
2023-12-09 21:52:08 +0100

    Day 4, part 2

--- 2023/04/solution.py
+++ 2023/04/solution.py
@@ -7,15 +7,28 @@ with open(input_file) as f:
 
 # Parse data
 total_points = 0
-for card in data.strip().split('\n'):
+card_wins = {}
+for c, card in enumerate(data.strip().split('\n')):
     _, numbers = card.split(':')
     winning_numbers_str, given_numbers_str = numbers.split('|')
     winning_numbers = set(int(s) for s in winning_numbers_str.split())
     given_numbers = set(int(s) for s in given_numbers_str.split())
 
     m = len(given_numbers & winning_numbers)
+    card_wins[c] = m
     if m > 0:
         total_points += 2**(m - 1)
 
 # Part 1: total number of points
 print(f"Total number of points: {total_points}")
+
+# Part 2: total number of cards
+cards = list(card_wins.keys())
+i = 0
+while i < len(cards):
+    c = cards[i]
+    m = card_wins[c]
+    cards.extend(c + j + 1 for j in range(m))
+    i += 1
+
+print(f"Total number of cards: {len(cards)}")
