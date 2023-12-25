0cecce85f8912bb44a53b7b778404e842e0d3300
Ludo Visser <ludo.visser@gmail.com>
2023-12-25 16:29:02 +0100

    Day 9, part 2

--- 2023/09/solution.py
+++ 2023/09/solution.py
@@ -15,19 +15,26 @@ for line in data.strip().split('\n'):
 def diff(h):
     return [h[i + 1] - h[i] for i in range(len(h) - 1)]
 
-extrapolated = []
+extrapolated_forward = []
+extrapolated_backwards = []
 for h in histories:
     # Compute differences until all zeroes
     diffs = [h, diff(h)]
     while any(diffs[-1]):
         diffs.append(diff(diffs[-1]))
 
-    # Append integrated value
+    # Extrapolate forward and backwards
     diffs[-1].append(0)
+    diffs[-1].insert(0, 0)
     for i in reversed(range(1, len(diffs))):
         diffs[i - 1].append(diffs[i - 1][-1] + diffs[i][-1])
+        diffs[i - 1].insert(0, diffs[i - 1][0] - diffs[i][0])
 
-    extrapolated.append(diffs[0][-1])
+    extrapolated_forward.append(diffs[0][-1])
+    extrapolated_backwards.append(diffs[0][0])
 
-# Part 1: sum of extrapolated values
-print(f"Sum of extrapolated values: {sum(extrapolated)}")
+# Part 1: sum of forward extrapolated values
+print(f"Sum of forward extrapolated values: {sum(extrapolated_forward)}")
+
+# Part 2: sum of backwards extrapolated values
+print(f"Sum of backwards extrapolated values: {sum(extrapolated_backwards)}")
