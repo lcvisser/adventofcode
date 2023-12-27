41a25f9195e885cafd792abaea6b684b6cb41b4f
Ludo Visser <ludo.visser@gmail.com>
2023-12-27 19:53:21 +0100

    Day 11, part 2

--- 2023/11/solution.py
+++ 2023/11/solution.py
@@ -25,32 +25,38 @@ for c in range(n_cols):
     if all(image[r][c] == '.' for r in range(n_rows)):
         empty_cols.append(c)
 
-# Expand image
-image_expanded = []
-for r, row in enumerate(image):
-    image_expanded.append([])
-    for c, ch in enumerate(row):
-        image_expanded[-1].append(ch)
-        if c in empty_cols:
-            image_expanded[-1].append('.')
-
-    if r in empty_rows:
-        image_expanded.append(['.' for _ in range(n_cols + len(empty_cols))])
-
 # Locate galaxies
 galaxies = dict()
 gid = 1
-for r, row in enumerate(image_expanded):
+for r, row in enumerate(image):
     for c, ch in enumerate(row):
         if ch == '#':
             galaxies[gid] = (r, c)
             gid += 1
 
 # Compute distances
-dists = []
-for a, b in itertools.combinations(galaxies.keys(), 2):
-    d = abs(galaxies[a][0] - galaxies[b][0]) + abs(galaxies[a][1] - galaxies[b][1])
-    dists.append(d)
+def compute_distances(galaxies, expansion_factor):
+    dists = []
+    for a, b in itertools.combinations(galaxies.keys(), 2):
+        xa = min(galaxies[a][0], galaxies[b][0])
+        xb = max(galaxies[a][0], galaxies[b][0])
+        nx = sum(1 for x in range(xa, xb) if x in empty_rows)
+        dx = xb - xa + nx * (expansion_factor - 1)  # do not count the empty rows twice, so -1
+
+        ya = min(galaxies[a][1], galaxies[b][1])
+        yb = max(galaxies[a][1], galaxies[b][1])
+        ny = sum(1 for y in range(ya, yb) if y in empty_cols)
+        dy = yb - ya + ny * (expansion_factor - 1)  # do not count the empty rows twice, so -1
+
+        d = dx + dy
+        dists.append(d)
+
+    return dists
 
 # Part 1: sum of distances
-print(f"Sum of distances: {sum(dists)}")
+dists1 = compute_distances(galaxies, 2)
+print(f"Sum of distances (expansion=2): {sum(dists1)}")
+
+# Part 2: sum of distances in expanded universe
+dists2 = compute_distances(galaxies, 1000000)
+print(f"Sum of distances (expansion=1000000): {sum(dists2)}")
