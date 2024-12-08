d6fdf323fc6b788fcef27887480929ee4d71a87d
Ludo Visser <ludo.visser@gmail.com>
2024-12-08 13:42:28 +0100

    Part 2

--- 2024/08/solution.py
+++ 2024/08/solution.py
@@ -20,7 +20,7 @@ for r, line in enumerate(data.strip().split('\n')):
             antennas[ch].append((r, c))
 
 # Find antinodes per antenna frequency
-antinodes = set()
+antinodes1 = set()
 for antenna_id, antenna_positions in antennas.items():
     for antenna1, antenna2 in itertools.combinations(antenna_positions, 2):
         x1, y1 = antenna1
@@ -30,11 +30,42 @@ for antenna_id, antenna_positions in antennas.items():
 
         antinode1 = (x1 - dx, y1 - dy)
         if 0 <= antinode1[0] <= height and 0 <= antinode1[1] <= width:
-            antinodes.add(antinode1)
+            antinodes1.add(antinode1)
 
         antinode2 = (x2 + dx, y2 + dy)
         if 0 <= antinode2[0] <= height and 0 <= antinode2[1] <= width:
-            antinodes.add(antinode2)
+            antinodes1.add(antinode2)
 
 # Part 1: number of unique antinodes
-print(f"number of unique antinodes: {len(antinodes)}")
+print(f"number of unique antinodes: {len(antinodes1)}")
+
+# Find all antinodes per antenna frequency
+antinodes2 = set()
+for antenna_id, antenna_positions in antennas.items():
+    for antenna1, antenna2 in itertools.combinations(antenna_positions, 2):
+        # Find all antinodes on the line
+        x1, y1 = antenna1
+        x2, y2 = antenna2
+        dx = x2 - x1
+        dy = y2 - y1
+
+        i = 0  # antennas are also antinodes
+        while True:
+            antinode1 = (x1 - i * dx, y1 - i * dy)
+            if 0 <= antinode1[0] <= height and 0 <= antinode1[1] <= width:
+                antinodes2.add(antinode1)
+                i += 1
+            else:
+                break
+
+        j = 0  # antennas are also antinodes
+        while True:
+            antinode2 = (x2 + j * dx, y2 + j * dy)
+            if 0 <= antinode2[0] <= height and 0 <= antinode2[1] <= width:
+                antinodes2.add(antinode2)
+                j += 1
+            else:
+                break
+
+# Part 2: number of all unique antinodes on the line
+print(f"number of all unique antinodes on the line: {len(antinodes2)}")
