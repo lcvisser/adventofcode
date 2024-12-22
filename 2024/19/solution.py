6973b1238558a92d9ac95ab6e42341b15808e8f4
Ludo Visser <ludo.visser@gmail.com>
2024-12-22 22:43:24 +0100

    Part 2

--- 2024/19/solution.py
+++ 2024/19/solution.py
@@ -1,3 +1,5 @@
+import functools
+
 # Read data
 input_file = "2024/19/input.txt"
 with open(input_file) as f:
@@ -13,33 +15,34 @@ for line in data.strip().split('\n'):
         continue
 
     if not sep:
-        available_towels = [x.strip() for x in line.split(',')]
+        available_towels = tuple(x.strip() for x in line.split(','))
     else:
         desired_towels.append(line.strip())
 
 
-# Depth first search for a possible combination
-def make_towel(desired, available):
-    is_possible = False
+# Memoized recursive depth-first search, because the individual towels in the in-progress towel are indistinguishable
+@functools.cache
+def make_towel(desired, current, available):
+    num_solutions = 0
+    if current == desired:
+        return 1
+    else:
+        for p in filter(lambda x: desired.startswith(current + x), available):
+            num_solutions += make_towel(desired, current + p, available)
 
-    to_check = list(filter(lambda x: desired.startswith(x), available))
-    while to_check:
-        towel = to_check.pop(0)
-        if towel == desired:
-            is_possible = True
-            break
+    return num_solutions
 
-        possible = list(filter(lambda x: desired.startswith(towel + x), available))
-        for p in possible:
-            to_check.insert(0, towel + p)
-
-    return is_possible
 
 # Check designs
 possible = 0
+num_patterns = 0
 for t in desired_towels:
-    if make_towel(t, available_towels):
+    if (n := make_towel(t, "", available_towels)) > 0:
         possible += 1
+        num_patterns += n
 
 # Part 1: number of possible designs
 print(f"Number of possible designs: {possible}")
+
+# Part 2: sum of possible design options
+print(f"Sum of number of possible patterns: {num_patterns}")
