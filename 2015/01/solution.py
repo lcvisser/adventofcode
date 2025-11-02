5d74375d888fd329b1bbec2aec26af573e7a159d
Ludo Visser <ludo.visser@gmail.com>
2025-11-02 18:11:08 +0100

    Part 2

--- 2015/01/solution.py
+++ 2015/01/solution.py
@@ -10,3 +10,19 @@ for c in instructions:
         current_floor -= 1
 
 print(f"Final floor: {current_floor}")
+
+# Part 2
+current_floor = 0
+target_floor = -1
+character_index = 0
+for i, c in enumerate(instructions):
+    if c == "(":
+        current_floor += 1
+    elif c == ")":
+        current_floor -= 1
+
+    if current_floor == target_floor:
+        character_index = i + 1  # starting index is 1
+        break
+
+print(f"First time to reach target floor at character: {character_index}")
