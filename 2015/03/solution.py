d79aa025a59df098025cf4a86dabb220df1858fe
Ludo Visser <ludo.visser@gmail.com>
2025-11-02 20:08:26 +0100

    Part 2

--- 2015/03/solution.py
+++ 2015/03/solution.py
@@ -20,3 +20,25 @@ for d in instructions:
 
 # Part 1
 print(f"Number of houses visited: {len(visited)}")
+
+santa_position = (0, 0)
+robot_position = (0, 0)
+visited = { santa_position, robot_position}
+santa_to_move = True
+for d in instructions:
+    dx, dy = moves[d]
+    if santa_to_move:
+        x, y = santa_position
+        new_position = x + dx, y + dy
+        visited.add(new_position)
+        santa_position = new_position
+    else:
+        x, y = robot_position
+        new_position = x + dx, y + dy
+        visited.add(new_position)
+        robot_position = new_position
+
+    santa_to_move = not santa_to_move
+
+# Part 2
+print(f"Number of houses with at least one visit: {len(visited)}")
