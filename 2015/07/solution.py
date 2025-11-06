68424ecfb34ea5bae3da2f4eb4d770dc2d229e81
Ludo Visser <ludo.visser@gmail.com>
2025-11-06 20:51:58 +0100

    Part 2

--- 2015/07/solution.py
+++ 2015/07/solution.py
@@ -1,56 +1,75 @@
 with open("2015/07/input.txt") as f:
     data = f.read()
 
-signals = {}
 
-# Preset constants (for e.g. connections like 1 AND a -> b)
-for i in range(2**16):
-    signals[str(i)] = i
+def connect(connections):
+    signals = {}
 
-connections = data.strip().split("\n")
-while connections:
-    connection = connections.pop(0)
-    src, dst = connection.split(" -> ")
+    # Preset constants (for e.g. connections like 1 AND a -> b)
+    for i in range(2**16):
+        signals[str(i)] = i
 
-    if "AND" in src:
-        inp1, inp2 = src.split(" AND ")
-        if inp1 in signals.keys() and inp2 in signals.keys():
-            signals[dst] = signals[inp1] & signals[inp2]
-        else:
-            connections.append(connection)
-    elif "OR" in src:
-        inp1, inp2 = src.split(" OR ")
-        if inp1 in signals.keys() and inp2 in signals.keys():
-            signals[dst] = signals[inp1] | signals[inp2]
-        else:
-            connections.append(connection)
-    elif "NOT" in src:
-        inp1 = src[len("NOT "):]
-        if inp1 in signals.keys():
-            signals[dst] = ~signals[inp1]
-        else:
-            connections.append(connection)
-    elif "LSHIFT" in src:
-        inp1, s = src.split(" LSHIFT ")
-        if inp1 in signals.keys():
-            signals[dst] = signals[inp1] << int(s)
-        else:
-            connections.append(connection)
-    elif "RSHIFT" in src:
-        inp1, s = src.split(" RSHIFT ")
-        if inp1 in signals.keys():
-            signals[dst] = signals[inp1] >> int(s)
-        else:
-            connections.append(connection)
-    else:
-        try:
-            signals[dst] = int(src)
-        except ValueError:
-            # Direct wire
-            if src in signals.keys():
-                signals[dst] = signals[src]
+    while connections:
+        connection = connections.pop(0)
+        src, dst = connection.split(" -> ")
+
+        if "AND" in src:
+            inp1, inp2 = src.split(" AND ")
+            if inp1 in signals.keys() and inp2 in signals.keys():
+                signals[dst] = signals[inp1] & signals[inp2]
             else:
                 connections.append(connection)
+        elif "OR" in src:
+            inp1, inp2 = src.split(" OR ")
+            if inp1 in signals.keys() and inp2 in signals.keys():
+                signals[dst] = signals[inp1] | signals[inp2]
+            else:
+                connections.append(connection)
+        elif "NOT" in src:
+            inp1 = src[len("NOT "):]
+            if inp1 in signals.keys():
+                signals[dst] = ~signals[inp1]
+            else:
+                connections.append(connection)
+        elif "LSHIFT" in src:
+            inp1, s = src.split(" LSHIFT ")
+            if inp1 in signals.keys():
+                signals[dst] = signals[inp1] << int(s)
+            else:
+                connections.append(connection)
+        elif "RSHIFT" in src:
+            inp1, s = src.split(" RSHIFT ")
+            if inp1 in signals.keys():
+                signals[dst] = signals[inp1] >> int(s)
+            else:
+                connections.append(connection)
+        else:
+            try:
+                # Direct value on wire
+                signals[dst] = int(src)
+            except ValueError:
+                if src in signals.keys():
+                    # Direct wire
+                    signals[dst] = signals[src]
+                else:
+                    # Try again later
+                    connections.append(connection)
+
+    return signals
+
+connections = data.strip().split("\n")
 
 # Part 1
-print(f"Value for wire a: {signals["a"] & 0xffff}")
+signals1 = connect(connections.copy())
+signal_a1 = signals1["a"] & 0xffff
+print(f"Value for wire a: {signal_a1}")
+
+# Part 2
+for i, c in enumerate(connections):
+    if c.endswith(" -> b"):
+        connections[i] = f"{signal_a1} -> b"
+        break
+
+signals2 = connect(connections)
+signal_a2 = signals2["a"] & 0xffff
+print(f"New value for wire a: {signal_a2}")
