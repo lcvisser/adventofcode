ac1ace9bf8922a3911ef651ad3811c2f7f14d548
Ludo Visser <ludo.visser@gmail.com>
2025-11-08 22:54:33 +0100

    Part 2

--- 2015/08/solution.py
+++ 2015/08/solution.py
@@ -10,3 +10,12 @@ for d in data.strip().split(b'\n'):
     char_length += len(s) - 2  # -2 for the enclosing quotes
 
 print(f"code_length - char_length = {code_length - char_length}")
+
+# Part 2
+code_length = 0
+encoded_length = 0
+for d in data.strip().split(b'\n'):
+    code_length += len(d)
+    encoded_length += len(d) + (d.count(b'"') + d.count(b'\\')) + 2  # " -> \" and \ -> \\; +2 for enclosing quotes
+
+print(f"encoded_length - code_length = {encoded_length - code_length}")
