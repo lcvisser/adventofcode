7aa5da0e674a382d86508c84dbd4bc07cf9c1b52
Ludo Visser <ludo.visser@gmail.com>
2025-12-04 21:36:52 +0100

    Part 2

--- 2025/01/solution.py
+++ 2025/01/solution.py
@@ -4,6 +4,8 @@ with open("2025/01/input.txt") as f:
     data = f.read()
 
 rotate = {"L": add, "R": sub}
+
+# Part 1
 dial = 50
 password = 0
 for instruction in data.strip().split("\n"):
@@ -12,5 +14,18 @@ for instruction in data.strip().split("\n"):
     dial = rotate[d](dial, n) % 100
     if dial == 0:
         password += 1
+
+print(f"The password is: {password}")
+
+# Part 2
+dial = 50
+password = 0
+for instruction in data.strip().split("\n"):
+    d = instruction[0]
+    n = int(instruction[1:])
+    for i in range(n):
+        dial = rotate[d](dial, 1) % 100
+        if dial == 0:
+            password += 1
 
 print(f"The password is: {password}")
