bd7088bbd3bcff66d4779971dfa552d6d8d91df6
Ludo Visser <ludo.visser@gmail.com>
2025-12-04 21:34:18 +0100

    Part 1

---
+++ 2025/01/solution.py
@@ -0,0 +1,16 @@
+from operator import add, sub
+
+with open("2025/01/input.txt") as f:
+    data = f.read()
+
+rotate = {"L": add, "R": sub}
+dial = 50
+password = 0
+for instruction in data.strip().split("\n"):
+    d = instruction[0]
+    n = int(instruction[1:])
+    dial = rotate[d](dial, n) % 100
+    if dial == 0:
+        password += 1
+
+print(f"The password is: {password}")
