bdae45168b5479c287fdc14e1412041a80b88cfb
Ludo Visser <ludo.visser@gmail.com>
2025-12-06 22:20:55 +0100

    Part 2

--- 2025/03/solution.py
+++ 2025/03/solution.py
@@ -1,19 +1,25 @@
 with open("2025/03/input.txt") as f:
     data = f.read()
 
-total_joltage = 0
-for bank in data.strip().split("\n"):
-    batteries = [int(v) for v in bank]
-    num_batteries = len(batteries)
-    joltage_tens = 0
-    joltage_ones = 0
-    for i, b in enumerate(batteries):
-        if b > joltage_tens and i < num_batteries - 1:
-            joltage_tens = b
-            joltage_ones = batteries[i + 1]
-        elif b > joltage_ones:
-            joltage_ones = b
+def compute_joltage(num_pos):
+    total_joltage = 0
+    for bank in data.strip().split("\n"):
+        batteries = [int(v) for v in bank]
+        joltage = [0] * num_pos
+        batteries_used = [None] * num_pos
+        for i, b in enumerate(batteries):
+            for j in range(num_pos):
+                if b > joltage[j] and i not in batteries_used[:j] and i < len(batteries) - (num_pos - j - 1):
+                    joltage[j:] = batteries[i:i + 1 + (num_pos - j - 1)]
+                    batteries_used[j:] = list(range(i, i + num_pos - j))
+                    break
 
-    total_joltage += 10 * joltage_tens + joltage_ones
+        total_joltage += sum(10**i * j for i, j in enumerate(reversed(joltage)))
 
-print(f"Total joltage: {total_joltage}")
+    return total_joltage
+
+# Part 1
+print(f"Total joltage for n= 2: {compute_joltage(2)}")
+
+# Part 2
+print(f"Total joltage for n=12: {compute_joltage(12)}")
