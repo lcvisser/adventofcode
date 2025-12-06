8e2b5183776c069a0a9934f0838625feaa3804eb
Ludo Visser <ludo.visser@gmail.com>
2025-12-06 16:11:29 +0100

    Part 2

--- 2025/02/solution.py
+++ 2025/02/solution.py
@@ -3,13 +3,26 @@ import re
 with open("2025/02/input.txt") as f:
     data = f.read()
 
-REPEATED_SEQUENCE = re.compile(r"([0-9]+)(\1)")
+# Part 1
+TWICE_REPEATED_SEQUENCE = re.compile(r"([0-9]+)(\1)")
 
-sum_of_invalid_ids = 0
+sum_of_invalid_ids1 = 0
 for rng in data.strip().split(","):
     lower, upper = rng.split("-")
     for i in range(int(lower), int(upper) + 1):
-        if REPEATED_SEQUENCE.fullmatch(str(i)):
-            sum_of_invalid_ids += i
+        if TWICE_REPEATED_SEQUENCE.fullmatch(str(i)):
+            sum_of_invalid_ids1 += i
 
-print(f"Sum of invalid IDs: {sum_of_invalid_ids}")
+print(f"Sum of invalid IDs: {sum_of_invalid_ids1}")
+
+# Part 2
+REPEATED_SEQUENCES = re.compile(r"([0-9]+)\1+")
+
+sum_of_invalid_ids2 = 0
+for rng in data.strip().split(","):
+    lower, upper = rng.split("-")
+    for i in range(int(lower), int(upper) + 1):
+        if REPEATED_SEQUENCES.fullmatch(str(i)):
+            sum_of_invalid_ids2 += i
+
+print(f"Sum of invalid IDs: {sum_of_invalid_ids2}")
