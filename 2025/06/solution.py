82353a191cb8cc3af1372d58b9f17f2f0d3ec455
Ludo Visser <ludo.visser@gmail.com>
2025-12-07 22:08:34 +0100

    Part 2

--- 2025/06/solution.py
+++ 2025/06/solution.py
@@ -17,3 +17,34 @@ for p in problems:
     grand_total += result
 
 print(f"Grand total: {grand_total}")
+
+# Part 2
+homework = data.strip("\n").split("\n")
+problems = zip(*homework)
+
+grand_total = 0
+operands = []
+operator = None
+for p in reversed(list(problems)):
+    if all(v == " " for  v in p):
+        continue
+
+    n = len(p)
+    digits = []
+    for i, v in enumerate(p):
+        if i < n - 1 and v != " ":
+            digits = [10 * d for d in digits]
+            digits.append(int(v))
+
+        if i == n - 2:
+            operands.append(sum(digits))
+            digits = []
+
+        if i == n - 1 and v in "+*":
+            operator = {"+": add, "*": mul}[v]
+            result = reduce(operator, operands)
+            grand_total += result
+            operands = []
+            operator = None
+
+print(f"Grand total: {grand_total}")
