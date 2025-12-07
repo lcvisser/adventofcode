87d00ec798584c6c2ce552b0d0b63f9b353f85b9
Ludo Visser <ludo.visser@gmail.com>
2025-12-07 13:23:50 +0100

    Part 2

--- 2025/04/solution.py
+++ 2025/04/solution.py
@@ -1,11 +1,13 @@
 with open("2025/04/input.txt") as f:
     data = f.read()
 
-grid = []
-for row in data.strip().split("\n"):
-    grid.append([c for c in row])
 
-def at(r, c):
+the_grid = []
+for row in data.strip().split("\n"):
+    the_grid.append([c for c in row])
+
+
+def at(grid, r, c):
     if r < 0 or r >= len(grid):
         return "."
     elif c < 0 or c >= len(grid[0]):
@@ -13,15 +15,43 @@ def at(r, c):
     else:
         return grid[r][c]
 
+
+def get_accessible_rolls(grid):
+    accessible_rolls = []
+    for r, _ in enumerate(the_grid):
+        for c, _ in enumerate(row):
+            neighbors = []
+            for dr, dc in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+                neighbors.append(at(the_grid, r + dr, c + dc))
+
+            if at(the_grid, r, c) == "@" and neighbors.count("@") < 4:
+                accessible_rolls.append((r, c))
+
+    return accessible_rolls
+
+
+def num_accessible_rolls(grid):
+    return len(get_accessible_rolls(grid))
+
+
 # Part 1
-accessible_rolls = 0
-for r, _ in enumerate(grid):
-    for c, _ in enumerate(row):
-        neighbors = []
-        for dr, dc in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
-            neighbors.append(at(r + dr, c + dc))
+print(f"Number of accessible rolls: {num_accessible_rolls(the_grid)}")
 
-        if at(r, c) == "@" and neighbors.count("@") < 4:
-            accessible_rolls += 1
 
-print(f"Number of accessible rolls: {accessible_rolls}")
+# Part 2
+def remove_rolls(grid, rolls):
+    for r, c in rolls:
+        grid[r][c] = "."
+
+    return grid
+
+
+def num_removable_rolls(grid):
+    removed_rolls = 0
+    while rolls := get_accessible_rolls(grid):
+        removed_rolls += len(rolls)
+        grid = remove_rolls(grid, rolls)
+
+    return removed_rolls
+
+print(f"Number of rolls removed: {num_removable_rolls(the_grid)}")
