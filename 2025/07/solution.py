2cd7a7d3ba341988f3316be29937a579cc0230f5
Ludo Visser <ludo.visser@gmail.com>
2025-12-25 17:09:42 +0100

    Part 2

--- 2025/07/solution.py
+++ 2025/07/solution.py
@@ -3,11 +3,14 @@ with open("2025/07/input.txt") as f:
 
 num_splits = 0
 beams = None
+paths = None
 for row in data.strip().split("\n"):
     for i, c in enumerate(row):
         if c == "S":
             beams = ["."] * len(row)
             beams[i] = "|"
+            paths = [0] * len(row)
+            paths[i] = 1
             break
 
         if c == "^" and beams[i] == "|":
@@ -15,5 +18,11 @@ for row in data.strip().split("\n"):
             beams[i - 1] = "|"
             beams[i] = "."
             beams[i + 1] = "|"
+            p = paths[i]
+            paths[i] = 0
+            paths[i - 1] += p
+            paths[i + 1] += p
+
 
 print(f"Number of splits: {num_splits}")
+print(f"Number of paths: {sum(paths)}")
